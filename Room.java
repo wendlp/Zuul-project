3e38f71756678826f68a483bf5da7b79c01d5436
wendlp <143395712+wendlp@users.noreply.github.com>
2023-08-30 00:20:18 -0300

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,69 @@
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+import java.util.HashMap;
+public class Room 
+{
+    public String description;
+    private HashMap<String, Romm> exits;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(String description) 
+    {
+        this.description = description;
+        exits = new HashMap<>();
+    }
+    
+     public String getLongDescription()
+    {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void setExit(String direction, Room neighbor) 
+    {
+        exits.put(direction, neighbor);
+    }
+
+    /**
+ * Return a string describing the room's exits, for example
+ * "Exits: north west".
+ */
+ public Room getExit(String direction)
+ {
+return exits.get(direction);
+}
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+}
